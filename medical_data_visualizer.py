e282627d459d74eeae3d8ada9e96a31b0eb4cf2b
Abiola Gbolahan Alabi <omogbolahan0001@gmail.com>
2025-05-19 00:41:16 -0800

    Update medical_data_visualizer.py

--- medical_data_visualizer.py
+++ medical_data_visualizer.py
@@ -12,17 +12,18 @@ df['overweight'] = (bmi > 25).astype(int)
 
 
 # 3
-df['cholesterol'] = df['cholesterol'].apply(lambd ax: 0 if x == 1 else 1)
+df['cholesterol'] = df['cholesterol'].apply(lambda x: 0 if x == 1 else 1)
 df['gluc'] = df['gluc'].apply(lambda x: 0 if x == 1 else 1)
 
 # 4
 def draw_cat_plot():
-    # 5
-    df_cat = None
+    # 5 Create DataFrame for cat plot using pd.melt
+    df_cat = pd.melt(df, id_vars=['cardio'], value_vars=['cholesterol', 'gluc', 'smoke', 'alco', 'active', 'overweight'])
 
 
-    # 6
-    df_cat = None
+    # 6 Group and reformat the data to show the counts of each feature
+    df_cat = df_cat.groupby(['cardio', 'variable', 'value']).size().reset_index(name='total')
+    
     
 
     # 7
